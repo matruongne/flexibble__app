d710933cd20bf8d07802b98ecf01e45ea8ecfd66
matruongne <matruong052003@gmail.com>
2023-07-23 21:34:05 +0700

    1s

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavLinks } from '../constants/index'
+import { AuthProviders } from '.'
+
+const Navbar = () => {
+	const session = null
+	return (
+		<nav className="flexBetween navbar">
+			<div className="flex-1 flexStart gap-10">
+				<Link href="/">
+					<Image src="/logo.svg" width={115} height={43} alt="Flexibble"></Image>
+				</Link>
+				<ul className="xl:flex hidden gap-7 text-small">
+					{NavLinks.map((link) => (
+						<Link href={link.href} key={link.key}>
+							{link.text}
+						</Link>
+					))}
+				</ul>
+			</div>
+			<div className="flexCenter gap-4">
+				{session ? (
+					<>
+						UserPhoto
+						<Link href="/create-project">Share Work</Link>
+					</>
+				) : (
+					<AuthProviders />
+				)}
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar'
+export { default as Footer } from './Footer'
+export { default as AuthProviders } from './AuthProviders'
+export { default as FooterColumn } from './FooterColumn'
